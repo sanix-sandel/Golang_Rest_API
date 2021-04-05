aa0d241468c8c1f4190acce8f6e613248ceca90a
sanix <sanicksikani@gmail.com>
2021-04-06 01:36:39 +0300

    Simple ServeMux

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"time"
 )
 
-func HealthCheck(w http.ResponseWriter, req *http.Request) {
-	currentTime := time.Now()
-	io.WriteString(w, currentTime.String())
-}
-
 func main() {
-	http.HandleFunc("/health", HealthCheck)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	mux := &UUID{}
+	http.ListenAndServe(":8000", mux)
 }
