7037f7853abc532f705e662e96d024115a0439c0
sanix <sanicksikani@gmail.com>
2021-04-06 02:59:07 +0300

    gorilla/mux

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	router := httprouter.New()
+//QueryHandler handles the given query parameters
+func QueryHandler(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
 
-	//Mapping to methods
-	router.ServeFiles("/static/*filepath", http.Dir("/home/sanix/tmp/static"))
-	log.Fatal(http.ListenAndServe(":8000", router))
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Got parameter id:%s!\n", queryParams["id"][0])
+	fmt.Fprintf(w, "Got parameter category:%s!", queryParams["category"][0])
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/articles", QueryHandler)
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
