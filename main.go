226725a4c37d9fa72e7188f03d2cce53290fd744
sanix <sanicksikani@gmail.com>
2021-04-06 10:48:58 +0300

    Gorilla handlers for Logging

--- main.go
+++ main.go
@@ -1,69 +1,23 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
-	"time"
+	"os"
 
-	"github.com/justinas/alice"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
-type city struct {
-	Name string
-	Area uint64
-}
-
-func filterContentType(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Currently in the check content type middleware")
-		if r.Header.Get("Content-type") != "application/json" {
-			w.WriteHeader(http.StatusUnsupportedMediaType)
-			w.Write([]byte("415 - Unsupported Meida Type. Please send JSON"))
-			return
-		}
-		handler.ServeHTTP(w, r)
-	})
-}
-
-func setServerTimeCookie(handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		handler.ServeHTTP(w, r)
-		//Setting cookie to every API response
-		cookie := http.Cookie{Name: "Server-Time(UTC)", Value: strconv.FormatInt(time.Now().Unix(), 10)}
-		http.SetCookie(w, &cookie)
-		log.Println("Currently in the set server time middleware")
-
-	})
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "POST" {
-		var tempCity city
-		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&tempCity)
-		if err != nil {
-			panic(err)
-		}
-
-		defer r.Body.Close()
-
-		fmt.Printf("Got %s city with area of %d sq miles!\n", tempCity.Name, tempCity.Area)
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("201 - Created"))
-
-	} else {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("405 - Method Not Allowed"))
-	}
+func handle(w http.ResponseWriter, r *http.Request) {
+	log.Println("Processing request!")
+	w.Write([]byte("OK"))
+	log.Println("Finished processing request")
 }
 
 func main() {
-
-	originalHandler := http.HandlerFunc(postHandler)
-	chain := alice.New(filterContentType, setServerTimeCookie).Then(originalHandler)
-	http.Handle("/city", chain)
-	http.ListenAndServe(":8000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", handle)
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	http.ListenAndServe(":8000", loggedRouter)
 }
