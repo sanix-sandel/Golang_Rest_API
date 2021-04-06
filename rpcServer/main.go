faa2b4d52270064083283edf9dd5d66ede287791
sanix <sanicksikani@gmail.com>
2021-04-06 11:31:29 +0300

    rpc json

--- rpcServer/main.go
+++ rpcServer/main.go
@@ -1,30 +1,68 @@
 package main
 
 import (
+	jsonparse "encoding/json"
+	"io/ioutil"
 	"log"
-	"net"
 	"net/http"
 	"net/rpc"
-	"time"
+	"os"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/rpc/json"
 )
 
-type Args struct{}
+type Args struct {
+	ID string
+}
 
-type TimeServer int64
+type Book struct {
+	ID     string `json:"id, omitempty"`
+	Name   string `json:"name, omitempty"`
+	Author string `json:"author, omitempty"`
+}
 
-func (t *TimeServer) GiveServerTime(args *Args, reply *int64) error {
-	*reply = time.Now().Unix()
+type JSONServer struct {
+}
+
+//Rpc implementation
+func (t *JSONServer) GiveBookDetail(r *http.Request, args *Args, reply *Book) error {
+	var books []Book
+	//Read Json file and load data
+	absPath, _ := filepath.Abs("rpcServer/books.json")
+	raw, readerr := ioutil.ReadFile(absPath)
+	if readerr != nil {
+		log.Println("error:", readerr)
+		os.Exit(1)
+	}
+
+	//Unmarshall Json raw data into books array
+	marshalerr := jsonparse.Unmarshal(raw, &books)
+	if marshalerr != nil {
+		log.Println("error:", marshalerr)
+		os.Exit(1)
+	}
+
+	//Iterate over each book to find the given book
+	for _, book := range books {
+		if book.ID == args.ID {
+			//if Book found, fill reply with it
+			*reply = book
+			break
+		}
+	}
 	return nil
 }
 
 func main() {
-	timeserver := new(TimeServer)
-	rpc.Register(timeserver)
-	rpc.HandleHTTP()
-	//Listen for requests on port 1234
-	l, e := net.Listen("tcp", ":1234")
-	if e != nil {
-		log.Fatal("listen error:", e)
-	}
-	http.Serve(l, nil)
+	//Create new Rpc server
+	s := rpc.NewServer()
+	//Register the type of data requested as JSON
+	s.RegisterCodec(json.NewCodec(), "application/json")
+	//Register the service by creating a new JSON server
+	s.RegisterService(new(JSONServer), "")
+	r := mux.NewRouter()
+	r.Handle("/rpc", s)
+	http.ListenAndServe(":1234", r)
 }
